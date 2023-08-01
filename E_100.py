8d4dd1c9da21534830dc04b139780d65d75f44ef
Paulina <100243639+PauSko@users.noreply.github.com>
2023-08-01 16:03:19 -0400

    Update E_100.py

--- E_100.py
+++ E_100.py
@@ -8,24 +8,17 @@
 from typing import Optional
 
 # Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
-
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        try:
-            if p.val == q.val and p.left.val == q.left.val and p.right.val == q.right.val:
-                return "true"
-            else:
-                print("false")
-        except:
-            print("false")
-
-# Example 1: Two identical binary trees
-p1 = TreeNode(1, TreeNode(2))
-q1 = TreeNode(1,None, TreeNode(2))
+        if p is None and q is None:
+            return True
+        if p is None or q is None:
+            return False
+        return p.val == q.val and self.isSameTree(p.left, q.left) and self.     isSameTree(p.right, q.right)
 
 print(Solution().isSameTree(p1, q1))
