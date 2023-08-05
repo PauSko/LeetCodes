d8ffa27d0fd93634e4d6c6c8670078ec359f3268
Paulina <100243639+PauSko@users.noreply.github.com>
2023-08-05 19:33:56 -0400

    Add files via upload

---
+++ E_136.py
@@ -0,0 +1,12 @@
+#E_136
+#Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+#You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+            unpaired_element = 0
+
+            for num in nums:
+                unpaired_element ^= num
+
+            return unpaired_element
